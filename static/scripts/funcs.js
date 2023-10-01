452a2413ca746778ddea15a11b02bc2b2ae1a79e
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2023-10-01 15:11:02 -0400

    remove icon (so ugly), remove sidebar (not used), add time

--- static/scripts/funcs.js
+++ static/scripts/funcs.js
@@ -5,3 +5,23 @@ function numberWithCommas(x) {
 function roundTwoDecimals(x) {
     return Math.round(x * 100) / 100;
 }
+
+function getTimeString() {
+    return new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+}
+
+function getDayString() {
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date().toLocaleDateString(undefined, options);
+}
+
+$(document).ready(function () {
+    // Update time loop
+    const updateLabels = () => {
+        $("#time-label").text(getTimeString());
+        $("#day-label").text(getDayString());
+    }
+
+    updateLabels();
+    setInterval(updateLabels, 200);
+});
